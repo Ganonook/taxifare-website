2a95d5c7929ce47f21bd2f866d77098ce73fb4ee
louis vidal <vidal-louis@hotmail.fr>
2026-08-21 16:01:07 +0200

    My first website

--- app.py
+++ app.py
@@ -1,6 +1,13 @@
 import streamlit as st
 import datetime
 import requests
+from streamlit_folium import st_folium
+import folium
+from geopy.geocoders import Nominatim
+from geopy.exc import GeocoderTimedOut, GeocoderServiceError
+
+
+
 
 # -----------------------------
 # Title
@@ -38,25 +45,25 @@ pickup_datetime_str = pickup_datetime.strftime("%Y-%m-%d %H:%M:%S")
 # Pickup coordinates
 # -----------------------------
 
-pickup_longitude = st.number_input(
-    "Pickup longitude",
-)
+# pickup_longitude = st.number_input(
+#     "Pickup longitude",
+# )
 
-pickup_latitude = st.number_input(
-    "Pickup latitude",
-)
+# pickup_latitude = st.number_input(
+#     "Pickup latitude",
+# )
 
 # -----------------------------
 # Dropoff coordinates
 # -----------------------------
 
-dropoff_longitude = st.number_input(
-    "Dropoff longitude",
-)
+# dropoff_longitude = st.number_input(
+#     "Dropoff longitude",
+# )
 
-dropoff_latitude = st.number_input(
-    "Dropoff latitude",
-)
+# dropoff_latitude = st.number_input(
+#     "Dropoff latitude",
+# )
 
 # -----------------------------
 # Passenger count
@@ -68,6 +75,53 @@ passenger_count = st.slider(
     max_value=10,
     value=3
 )
+
+
+#-------------
+#on est ou on vas ou
+#-----------------
+
+
+depart = st.text_input("📍 adresse de départ", "26 avenue d'eylau 75016 paris")
+arrive = st.text_input("🏁 adresse d'arriver" ," 88 rue du Faubourg Saint-Honoré")
+
+
+
+# # center on Liberty Bell, add marker
+# m = folium.Map(location=[pickup_longitude, pickup_latitude], zoom_start=16)
+
+# # call to render Folium map in Streamlit
+# st_data = st_folium(m, width=725)
+
+
+geolocator = Nominatim(user_agent="taxifare", timeout=10)
+
+@st.cache_data
+def geocode_cached(address):
+    geolocator = Nominatim(user_agent="taxifare_app_louis_vidal", timeout=10)
+    try:
+        return geolocator.geocode(address)
+    except (GeocoderTimedOut, GeocoderServiceError):
+        return None
+
+pickup_location = geocode_cached(depart)
+destination_location = geocode_cached(arrive)
+
+
+
+
+if pickup_location and destination_location:
+    pickup_lat, pickup_lon = pickup_location.latitude, pickup_location.longitude
+    destination_lat, destination_lon = destination_location.latitude, destination_location.longitude
+
+    m = folium.Map(location=[pickup_lat, pickup_lon], zoom_start=13)
+    folium.Marker([pickup_lat, pickup_lon], tooltip="Départ").add_to(m)
+    folium.Marker([destination_lat, destination_lon], tooltip="Arrivée", icon=folium.Icon(color="red")).add_to(m)
+
+    st_folium(m, width=725)
+else:
+    st.warning("Adresse de départ ou d'arrivée introuvable.")
+
 
 # -----------------------------
 # API
@@ -83,10 +137,10 @@ if st.button("Predict fare"):
 
     voyage = {
         "pickup_datetime": pickup_datetime_str,
-        "pickup_longitude": pickup_longitude,
-        "pickup_latitude": pickup_latitude,
-        "dropoff_longitude": dropoff_longitude,
-        "dropoff_latitude": dropoff_latitude,
+        "pickup_longitude": pickup_lon,
+        "pickup_latitude": pickup_lat,
+        "dropoff_longitude": destination_lon,
+        "dropoff_latitude": destination_lat,
         "passenger_count": passenger_count
     }
 
